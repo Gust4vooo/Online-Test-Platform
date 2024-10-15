0b52515d0db748dc0aa6807b9df86078ee63ca44
Gustavo <gusty.risandha@gmail.com>
2024-10-15 21:00:42 +0800

    get data test pada pembayaran

--- backend/src/routes/testRoutes.js
+++ backend/src/routes/testRoutes.js
@@ -1,20 +1,17 @@
 import express from 'express';
+<<<<<<< HEAD
 import { createTest, getTest } from '../controllers/testControllers.js';
+=======
+import { createTest, getTestDetail } from '../controllers/testControllers.js';
+>>>>>>> 67c6347a (get data test pada pembayaran)
 
 const router = express.Router();
 
 router.post('/create-test', createTest);
+<<<<<<< HEAD
 router.get('/get-test/:id', getTest);
+=======
+router.get('/test-detail/:testId', getTestDetail);
+>>>>>>> 67c6347a (get data test pada pembayaran)
 
 export default router; // Menggunakan default export
-
-
-
-// const express = require("express");
-// const { createTest } = require("backend/src/controllers/testControllers.js");
-
-// const router = express.Router();
-
-// router.post("/create-test", createTest);
-
-// module.exports = router;
